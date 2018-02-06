211eafd61920877e0a2c6ce72bf2bd5a03ffe6e2
Aditya Agarwal <36186097+adihaina@users.noreply.github.com>
2018-02-06 14:59:18 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello
+  {
+   public static void main(String args[])
+    {
+    System.out.println("Hello Amit Singh");
+    System.out.println("I have just started using Github");
+    System.out.println("It feels good.");
+    }
+  }
